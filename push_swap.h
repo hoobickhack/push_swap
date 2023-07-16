eb75b07c02161aa46d9a211608eff92410cb7d7f
Ilyes Souaidia <isouaidi@c1r5p1.42nice.fr>
2023-07-16 17:23:23 +0200

    2

---
+++ push_swap.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isouaidi <isouaidi@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/13 19:13:08 by isouaidi          #+#    #+#             */
+/*   Updated: 2023/07/13 19:15:17 by isouaidi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+# include "../../libft/inc/libft.h"
+# include "../../libft/inc/ft_printf.h"
+# include "../../libft/inc/get_next_line_bonus.h"
+
+#endif
