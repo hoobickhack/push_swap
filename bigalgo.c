055bdd674f1a96a059f49a2cdce8a7648f75a4c0
newpc <leilyesdu06300@gmail.com>
2024-01-28 20:42:38 +0100

    final

--- bigalgo.c
+++ bigalgo.c
@@ -6,7 +6,7 @@
 /*   By: isouaidi <isouaidi@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 19:11:44 by isouaidi          #+#    #+#             */
-/*   Updated: 2024/01/28 01:28:04 by isouaidi         ###   ########.fr       */
+/*   Updated: 2024/01/28 20:40:51 by isouaidi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	bigalgo(t_stack **st, t_stack **st2, t_truct *truct, int i)
 		algo(i, st, st2, truct->sizet);
 		while (*st2 != NULL)
 		{
-			push(st2, st);
+			push(*(&st2), *(&st));
 			ft_printf("pa\n");
 		}
 		i++;
@@ -47,7 +47,7 @@ void	algo(int i, t_stack **st, t_stack **st2, int size)
 		}
 		else
 		{
-			push(st, st2);
+			push(*(&st), *(&st2));
 			ft_printf("pb\n");
 		}
 		j++;
